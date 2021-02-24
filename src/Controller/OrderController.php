a130114798b094b2f64aa4aa559f6a46bfef4097
Alexandre C <contact@acanoen.fr>
2021-02-24 17:22:03 +0100

    Add orders tunnel

---
+++ src/Controller/OrderController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Controller;
+
+use App\Classe\Cart;
+use App\Entity\Order;
+use App\Entity\OrderDetails;
+use App\Form\OrderType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OrderController extends AbstractController
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @Route("/commande", name="order")
+     */
+    public function index(Request $request, Cart $cart): Response
+    {
+        if (!$this->getUser()->getAddresses()->getValues()) {
+            return $this->redirectToRoute('account_address_add');
+        }
+
+        $form = $this->createForm(OrderType::class, null, [
+            'user' => $this->getUser()
+        ]);
+
+        return $this->render('order/index.html.twig', [
+            'form' => $form->createView(),
+            'cart' => $cart->getFull()
+        ]);
+    }
+
+    /**
+     * @Route("/commande/recapitulatif", name="order_recap", methods={"POST"})
+     */
+    public function add(Request $request, Cart $cart): Response
+    {
+        $form = $this->createForm(OrderType::class, null, [
+            'user' => $this->getUser()
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $date = new \DateTime();
+            $carriers = $form->get('carriers')->getData();
+            $delivery = $form->get('addresses')->getData();
+            $delivery_content = $delivery->getFirstname().' '.$delivery->getLastname();
+            $delivery_content .= '<br/>'.$delivery->getPhone();
+            if ($delivery->getCompany()) {
+                $delivery_content .= '<br/>'.$delivery->getCompany();
+            }
+            $delivery_content .= '<br/>'.$delivery->getAddress();
+            $delivery_content .= '<br/>'.$delivery->getPostal().' '.$delivery->getCity();
+            $delivery_content .= '<br/>'.$delivery->getCountry();
+
+            // Save Order
+            $order = new Order();
+            $order->setUser($this->getUser());
+            $order->setCreatedAt($date);
+            $order->setCarrierName($carriers->getName());
+            $order->setCarrierPrice($carriers->getPrice());
+            $order->setDelivery($delivery_content);
+            $order->setIsPaid(0);
+
+            $this->entityManager->persist($order);
+
+            // Save Order Details
+            foreach ($cart->getFull() as $item) {
+                $orderDetails = new OrderDetails();
+                $orderDetails->setMyOrder($order);
+                $orderDetails->setProduct($item['product']->getName());
+                $orderDetails->setPrice($item['product']->getPrice());
+                $orderDetails->setQuantity($item['quantity']);
+                $orderDetails->setTotal($item['product']->getPrice() * $item['quantity']);
+                $this->entityManager->persist($orderDetails);
+            }
+
+            $this->entityManager->flush();
+
+            return $this->render('order/add.html.twig', [
+                'cart' => $cart->getFull(),
+                'carrier' => $carriers,
+                'delivery' => $delivery_content
+            ]);
+        }
+
+        return $this->redirectToRoute('cart');
+    }
+}
