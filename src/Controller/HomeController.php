937c925ef8bdcae2576f0a581fddcc1a898a2c2e
Alexandre C <contact@acanoen.fr>
2021-02-25 14:19:59 +0100

    Add headers

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Header;
 use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -29,8 +30,13 @@ class HomeController extends AbstractController
             ->getRepository(Product::class)
             ->findByIsBest(1);
 
+        $headers = $this->entityManager
+            ->getRepository(Header::class)
+            ->findAll();
+
         return $this->render('home/index.html.twig', [
-            'products' => $products
+            'products' => $products,
+            'headers' => $headers
         ]);
     }
 }
